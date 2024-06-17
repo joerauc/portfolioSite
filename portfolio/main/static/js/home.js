80f92a7eccff1f8a216e458ac2c72e0923293616
Joseph Raucci <joeraucci97@gmail.com>
2024-06-17 17:16:43 -0500

    First commit

---
+++ portfolio/main/static/js/home.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const nameSearch = document.getElementById("name-search")
+    const tags = document.querySelectorAll(".tag")
+    const projects = document.querySelectorAll(".project")
+
+    function filterProjects() {
+        const nameQuery = nameSearch.ariaValueMax.toLowerCase();
+
+        
+    }
+}) /* Only do function once DOM content is loaded */
---
+++ portfolio/main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Project, Tag
+
+# A view is a function called when going to a specific root
+def home(request):
+    projects = Project.objects.all() # uses ORM from django, get all made projects
+    tags = Tag.objects.all()
+    return render(request, "home.html", {"projects": projects, "tags": tags})
+
+def contact(request):
+    return render(request, "contact.html")
+
+def project(request, id):
+    return render(request, "project.html")
